0d802f6c0ba8150c8d4e7e34dba4ef493b60e300
Jason Zhao <jasonnope123@gmail.com>
2022-10-14 14:38:26 -0700

    inventory update

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     let embed = new Discord.EmbedBuilder()
     .setTitle("Commands")
     .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
-    .addFields( {name: 'User', value: '\`start\` - Generate your profile\n\`profile\` - User profile information\n\`balance\` - w.i.p'},
+    .addFields( {name: 'User', value: '\`start\` - Generate your profile\n\`profile\` - User profile information\n\`inventory(inv)\` - Characters owned list\n\`balance\` - w.i.p'},
                 {name: 'Admin', value: '\`purge\` - Mass remove messages '})
     return message.channel.send({embeds: [embed]})
 
--- commands/inventory.js
+++ commands/inventory.js
@@ -1,10 +1,52 @@
+const mongoose = require("mongoose");
 const botconfig = require("../config.json");
 const Discord = require("discord.js");
+const fs = require('fs')
 
+//connect to db
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const Data = require("../models/data.js");
 
 
 module.exports.run = async (client, message, args) => {
-    
+    Data.findOne({
+        userID: message.author.id 
+    }, (err, data) => {
+        // Check if user started game
+        if (!data) {
+            let WarningEmbed = new Discord.EmbedBuilder()
+            .setColor('#D62828')
+            .setTitle("You need to create a profile")
+            message.channel.send({embeds: [WarningEmbed]})
+            return;
+        }
+        if(!args[0])
+        {
+            data.characters.sort((a,b) => (a.id > b.id) ? 1: -1);
+            let invString = ``
+            if(data.characters.length > 0)
+            {
+                for (let i = 0; i < data.characters.length; i++) {
+                    invString += `\`${data.characters[i].name} (id: ${data.characters[i].id})\` - Amount (x${data.characters[i].issues.length})\n `
+                }
+                let embed = new Discord.EmbedBuilder()
+                .setColor('#D62828')
+                .addFields(
+                    { name: '💫 Inventory', value: invString, inline: true},
+                )
+                message.channel.send({embeds: [embed]})
+            }
+        }else if(!isNaN(args[0])){
+            
+        }
+
+
+        return;
+    });
 }
 
 module.exports.help = {
